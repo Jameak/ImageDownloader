d6cc59ef5afff534b3a39bf620043880b03b2e71
Marcus Winding Quistgaard <marcuswquistgaard@gmail.com>
2018-05-22 23:44:25 +0200

    Fix auto-updater
    After Github disabled TLS1.1 connections in february, the auto-updater has
    been unable to get a connection to check for updates.
    
    The HttpClient in .NET 4.5.2 defaults to TLS1.1; upgrading to .NET 4.6.2
    upgrades the default protocol used by HttpClient.

--- src/DataAccess/Properties/Settings.Designer.cs
+++ src/DataAccess/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DataAccess.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -130,7 +130,7 @@ namespace DataAccess.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v1.1.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v1.1.2")]
         public string VersionNumber {
             get {
                 return ((string)(this["VersionNumber"]));
